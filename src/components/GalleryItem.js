14f6fdff03628ee935ee70a339b1fabf63c797c1
unknown <v.sukanya@gmail.com>
2018-01-25 12:56:12 -0700

    added responsive CSS

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -7,12 +7,14 @@ export default class GalleryItem extends Component {
         return (this.props.entry.type === "photo" ?
                                 
             <div className="entry-holder">
+            <div className="trans-wrap">
                 <div className="entry-body">
                     <img src={this.props.entry.image} alt={this.props.entry.title}/>
                 </div>
                 <b>{this.props.entry.title}</b><br/>
                 {this.props.entry.location}, {this.props.entry.year} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 <Link to={`/dashboard/viewer/${this.props.entry.eid}`}>bigger</Link>
+            </div>
             </div>
         :
             <div className="entry-holder">
